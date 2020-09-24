8324c3e7a1a15d53898e78420a5e6028b5a82d4f
Volodymyr Ostapyshyn <ostapyshyn@gmail.com>
2020-09-24 14:41:22 +0300

    Initial commit, Add Basic List

---
+++ EmailAttachment/AttachmentTableViewController.swift
@@ -0,0 +1,50 @@
+//
+//  AttachmentTableViewController.swift
+//  EmailAttachment
+//
+//  Created by Simon Ng on 5/10/2016.
+//  Copyright © 2016 AppCoda. All rights reserved.
+//
+
+import UIKit
+import MessageUI
+
+class AttachmentTableViewController: UITableViewController {
+
+    let filenames = ["10 Great iPhone Tips.pdf", "camera-photo-tips.html", "foggy.jpg", "Hello World.ppt", "no more complaint.png", "Why Appcoda.doc"]
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Enable large title
+        navigationController?.navigationBar.prefersLargeTitles = true
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+    // MARK: - Table view data source
+
+    override func numberOfSections(in tableView: UITableView) -> Int {
+        // Return the number of sections
+        return 1
+    }
+
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        // Return the number of rows
+        return filenames.count
+    }
+
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "Cell", for: indexPath)
+
+        // Configure the cell...
+        cell.textLabel?.text = filenames[indexPath.row]
+        cell.imageView?.image = UIImage(named: "icon\(indexPath.row)");
+
+        return cell
+    }
+
+}
